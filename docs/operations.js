95ad920cb2ff6e98521573337de50c4815841537
bykeal <okonkwochibuike81@gmail.com>
2022-07-07 11:09:58 +0100

    intial start

---
+++ docs/operations.js
@@ -0,0 +1,100 @@
+let Amount ;
+let Balance = 0;
+let rateamount = 0;
+let flatarray = [];
+let ratioarray = [];
+let percentarray = [];
+let resdata =[];
+
+const calculateflat = ({SplitValue}) =>{
+	Amount = Amount - SplitValue;
+	console.log(Amount);
+	return SplitValue;
+}
+
+const calculatepercent = ({SplitValue}) =>{
+	const percent = (SplitValue/100)* Amount;
+	Amount = Amount - percent;
+	console.log(Amount);
+	return percent;
+}
+const calculateratio = (SplitValue, totalratio) =>{
+	const share = (SplitValue/totalratio)*Amount;
+	rateamount += share;
+	Balance = Amount - share;
+	console.log(Balance);
+	return share;
+	
+}
+const totalratios = (arr) => {
+	let acc = 0;
+	for (let i = 0; i < arr.length; i++) {
+		acc += arr[i].SplitValue;
+	}
+	return acc;
+}
+
+
+
+
+const operations = async (data) => {
+    Amount = data.Amount;
+    const spiltarray = data.SplitInfo;
+    console.log(spiltarray);
+
+    for (let i = 0; i < spiltarray.length; i++) {
+        if (spiltarray[i]["SplitType"].includes("FLAT")) {
+            flatarray.push(spiltarray[i]);
+        }
+        if (spiltarray[i]["SplitType"].includes("RATIO")) {
+            ratioarray.push(spiltarray[i]);
+        }
+        if (spiltarray[i]["SplitType"].includes("PERCENTAGE")) {
+            percentarray.push(spiltarray[i]);
+        }		
+    }
+    
+    for (let i = 0; i < flatarray.length; i++) {
+        // calculateflat(flatarray[i]);
+        resdata.push( {
+            "SplitEntityId": flatarray[i].SplitEntityId,
+            "Amount": calculateflat(flatarray[i])
+        })
+    
+    }
+
+
+    for (let i = 0; i < percentarray.length; i++) {
+        // calculatepercent(percentarray[i]);
+        resdata.push( {
+            "SplitEntityId": percentarray[i].SplitEntityId,
+            "Amount": calculatepercent(percentarray[i])
+        })
+    }
+
+
+    const rate = totalratios(ratioarray);
+    for (let i = 0; i < ratioarray.length; i++) {
+        let result = calculateratio(ratioarray[i].SplitValue, rate);
+        resdata.push( {
+            "SplitEntityId": ratioarray[i].SplitEntityId,
+            "Amount": result
+        })
+    }
+
+    const Finalbalance = Amount-rateamount;
+
+    return { resdata, Finalbalance };
+}
+
+
+
+
+
+
+
+
+
+module.exports = {
+    operations,
+}
---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const dotenv = require("dotenv");
+const { operations } = require("./docs/operations");
+const app = express();
+const bodyParser = require('body-parser');
+dotenv.config();
+
+app.use(bodyParser.json());
+
+//middleware
+app.post("/split-payments/compute", async (req,res) => {
+    if(req.body.SplitInfo.length > 0){
+        if(req.body.SplitInfo.length > 20){
+            res.status = 200;
+            res.json({response: 'the SplitInfo array must exceed 20 elements '});
+        }else{
+
+            const { resdata, Finalbalance} = await operations(req.body);
+            // console.log(req.body.SplitInfo);
+            const response = {
+                ID: req.body["ID"],
+                Balance: Finalbalance,
+                SplitBreakdown: resdata
+            }
+            res.status = 200;
+            res.json({response});
+
+        }
+    }else{
+        res.status = 200;
+        res.json({response: 'the SplitInfo array must contain an element '});
+    }
+   
+});
+
+app.use((req,res,next) => {
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err,req,res,next) => {
+    res.status(err.status || 500);
+    res.json({success:"false" , error:err.message})
+});
+
+
+
+
+
+
+
+app.listen(8800, () => {
+    console.log('live');
+});
